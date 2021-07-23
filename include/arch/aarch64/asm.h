0fe334e13afeed66c0d54e63f68c0a7e5d54aa39
Shunsuke Mie <sux2mfgj@gmail.com>
2021-07-23 20:04:48 +0900

    apply clang-format

--- include/arch/aarch64/asm.h
+++ include/arch/aarch64/asm.h
@@ -4,66 +4,79 @@
 
 #include <core/types.h>
 
-static inline void write_tcr_el2(u64 value)
+static inline void
+write_tcr_el2 (u64 value)
 {
-    asm volatile ("msr tcr_el2, %[x]" ::[x]"r"(value));
+	asm volatile("msr tcr_el2, %[x]" ::[x] "r"(value));
 }
 
-static inline void write_mair_el2(u64 value)
+static inline void
+write_mair_el2 (u64 value)
 {
-    asm volatile ("msr mair_el2, %[x]" ::[x]"r"(value));
+	asm volatile("msr mair_el2, %[x]" ::[x] "r"(value));
 }
 
-static inline u64 read_sctlr_el2(void)
+static inline u64
+read_sctlr_el2 (void)
 {
-    u64 value;
-    asm volatile ("mrs %[x], sctlr_el2" : [x]"=r"(value));
-    return value;
+	u64 value;
+	asm volatile("mrs %[x], sctlr_el2" : [x] "=r"(value));
+	return value;
 }
 
-static inline void write_sctlr_el2(u64 value)
+static inline void
+write_sctlr_el2 (u64 value)
 {
-    asm volatile ("msr sctlr_el2, %[x]" ::[x]"r"(value));
+	asm volatile("msr sctlr_el2, %[x]" ::[x] "r"(value));
 }
 
-static inline void write_ttbr0_el2(u64 value)
+static inline void
+write_ttbr0_el2 (u64 value)
 {
-    asm volatile ("msr ttbr0_el2, %[x]" ::[x]"r"(value));
+	asm volatile("msr ttbr0_el2, %[x]" ::[x] "r"(value));
 }
 
-static inline void isb(void)
+static inline void
+isb (void)
 {
-    asm volatile ("isb");
+	asm volatile("isb");
 }
 
-static inline void write_vbar_el2(u64 value)
+static inline void
+write_vbar_el2 (u64 value)
 {
-    asm volatile ("msr vbar_el2, %[x]" ::[x]"r"(value));
+	asm volatile("msr vbar_el2, %[x]" ::[x] "r"(value));
 }
 
-#define DEF_SYS_REG_READ(name) \
-    static inline u64 read_##name(void) \
-    { u64 value; asm volatile ("mrs %[x], "#name : [x]"=r"(value)); return value; }
+#define DEF_SYS_REG_READ(name)                                      \
+	static inline u64 read_##name (void)                        \
+	{                                                           \
+		u64 value;                                          \
+		asm volatile("mrs %[x], " #name : [x] "=r"(value)); \
+		return value;                                       \
+	}
 
-#define DEF_SYS_REG_WRITE(name) \
-    static inline void write_##name(u64 value) \
-    { asm volatile ("msr "#name", %[x]" ::[x]"r"(value));}
+#define DEF_SYS_REG_WRITE(name)                                       \
+	static inline void write_##name (u64 value)                   \
+	{                                                             \
+		asm volatile("msr " #name ", %[x]" ::[x] "r"(value)); \
+	}
 
-DEF_SYS_REG_READ(hcr_el2)
-DEF_SYS_REG_WRITE(hcr_el2)
-DEF_SYS_REG_READ(tpidr_el2)
-DEF_SYS_REG_WRITE(tpidr_el2)
+DEF_SYS_REG_READ (hcr_el2)
+DEF_SYS_REG_WRITE (hcr_el2)
+DEF_SYS_REG_READ (tpidr_el2)
+DEF_SYS_REG_WRITE (tpidr_el2)
 
-#define daif_clear(value) asm volatile ("msr daifclr, %0" :: "I"(value));
-#define daif_set(value) asm volatile ("msr daifset, %0" :: "I"(value))
+#define daif_clear(value) asm volatile("msr daifclr, %0" ::"I"(value));
+#define daif_set(value) asm volatile("msr daifset, %0" ::"I"(value))
 
-static inline void asm_set_stack_and_jump(ulong sp, void *jmpto)
+static inline void
+asm_set_stack_and_jump (ulong sp, void *jmpto)
 {
-    asm volatile (
-        "mov sp, %0\n\t"
-        "mov x30, %1\n\t"
-        "ret\n\t"
-        ::"r"(sp), "r"(jmpto));
+	asm volatile("mov sp, %0\n\t"
+		     "mov x30, %1\n\t"
+		     "ret\n\t" ::"r"(sp),
+		     "r"(jmpto));
 }
 
 static inline ulong
@@ -71,10 +84,10 @@ asm_lock_ulong_swap (ulong *mem, ulong newval)
 {
 	ulong oldval;
 
-    asm volatile (
-            "cas %0, %2, %1"
-            : "=r"(oldval), "+Q"(*mem) : "r"(newval), "+Q"(*mem));
-    return oldval;
+	asm volatile("cas %0, %2, %1"
+		     : "=r"(oldval), "+Q"(*mem)
+		     : "r"(newval), "+Q"(*mem));
+	return oldval;
 }
 
 #endif
