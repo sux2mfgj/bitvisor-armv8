d6e15a78e1f2256b98c7889767d796e8b60a505c
Shunsuke Mie <sux2mfgj@gmail.com>
2021-07-24 11:16:53 +0900

    formatting

--- core/panic.c
+++ core/panic.c
@@ -1,23 +1,22 @@
 
-#include "types.h"
 #include "printf.h"
+#include "types.h"
 
 /* print a message and stop */
-void __attribute__ ((noreturn))
-panic (char *format, ...)
+void __attribute__ ((noreturn)) panic (char *format, ...)
 {
 	va_list ap;
 	va_start (ap, format);
 	vprintf (format, ap);
 	va_end (ap);
 
-    //TODO
-    while(true)
-        asm volatile ("wfi");
+	// TODO
+	while (true)
+		asm volatile("wfi");
 }
 
 void
 panic_test (void)
 {
-    //TODO
+	// TODO
 }
