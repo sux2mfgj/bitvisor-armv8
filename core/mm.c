97238a4d58689230732d76911fff2f8dbedd1b39
Shunsuke Mie <sux2mfgj@gmail.com>
2021-08-08 18:56:55 +0900

    formatting

--- core/mm.c
+++ core/mm.c
@@ -27,20 +27,22 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <core.h>
 #include "mm.h"
-#include "list.h"
-#include "initfunc.h"
+
+#include <core.h>
+
 #include "assert.h"
+#include "initfunc.h"
+#include "list.h"
 #include "panic.h"
 
-#define NUM_OF_ALLOCSIZE	13
-#define NUM_OF_SMALL_ALLOCSIZE	5
-#define SMALL_ALLOCSIZE(n)	(0x80 << (n))
-#define NUM_OF_TINY_ALLOCSIZE	3
-#define TINY_ALLOCSIZE(n)	(0x10 << (n))
-#define FIND_NEXT_BIT(bitmap)	(~(bitmap) & ((bitmap) + 1))
-#define BIT_TO_INDEX(bit)	(__builtin_ffs ((bit)) - 1)
+#define NUM_OF_ALLOCSIZE 13
+#define NUM_OF_SMALL_ALLOCSIZE 5
+#define SMALL_ALLOCSIZE(n) (0x80 << (n))
+#define NUM_OF_TINY_ALLOCSIZE 3
+#define TINY_ALLOCSIZE(n) (0x10 << (n))
+#define FIND_NEXT_BIT(bitmap) (~(bitmap) & ((bitmap) + 1))
+#define BIT_TO_INDEX(bit) (__builtin_ffs ((bit)) - 1)
 
 enum page_type {
 	PAGE_TYPE_FREE,
@@ -83,7 +85,7 @@ virt_to_page (virt_t virt)
 	unsigned int i;
 
 	i = (virt - (virt_t)vmm_start_phys) >> PAGESIZE_SHIFT;
-	//ASSERT (i < NUM_OF_PAGES);
+	// ASSERT (i < NUM_OF_PAGES);
 	return &pagestruct[i];
 }
 
@@ -112,21 +114,23 @@ page_to_virt (struct page *p)
 	return phys_to_virt (page_to_phys (p));
 }
 
-u32 vmm_start_inf()
+u32
+vmm_start_inf ()
 {
-        return vmm_start_phys ;
+	return vmm_start_phys;
 }
 
-u32 vmm_term_inf()
+u32
+vmm_term_inf ()
 {
-        return vmm_start_phys+VMMSIZE_ALL ;
+	return vmm_start_phys + VMMSIZE_ALL;
 }
 
 static int
 mm_page_get_allocsize (int n)
 {
-	//ASSERT (n >= 0);
-	//ASSERT (n < NUM_OF_ALLOCSIZE);
+	// ASSERT (n >= 0);
+	// ASSERT (n < NUM_OF_ALLOCSIZE);
 	return 4096 << n;
 }
 
@@ -157,11 +161,11 @@ static struct page *
 mm_page_free_2ndhalf (struct page *p)
 {
 	int n = p->allocsize;
-	//ASSERT (n > 0);
+	// ASSERT (n > 0);
 	n--;
 	virt_t virt = page_to_virt (p);
 	int s = mm_page_get_allocsize (n);
-	//ASSERT (!(virt & s));
+	// ASSERT (!(virt & s));
 	p->allocsize = n;
 	struct page *q = virt_to_page (virt | s);
 	q->type = PAGE_TYPE_FREE;
@@ -187,25 +191,25 @@ static struct page *
 mm_page_alloc (int n)
 {
 	struct page *p;
-	//enum page_type old_type;
+	// enum page_type old_type;
 
-	//ASSERT (n < NUM_OF_ALLOCSIZE);
-	//spinlock_lock (&mm_lock);
+	// ASSERT (n < NUM_OF_ALLOCSIZE);
+	// spinlock_lock (&mm_lock);
 	p = mm_page_alloc_sub (n);
 	if (!p) {
-		//spinlock_unlock (&mm_lock);
-		//panic ("%s(%d): Out of memory", __func__, n);
+		// spinlock_unlock (&mm_lock);
+		// panic ("%s(%d): Out of memory", __func__, n);
 	}
 	/* p->type must be set before unlock, because the
 	 * mm_page_free() function may merge blocks if the type is
 	 * PAGE_TYPE_FREE. */
-	//old_type = p->type;
+	// old_type = p->type;
 	p->type = PAGE_TYPE_ALLOCATED;
-	//spinlock_unlock (&mm_lock);
+	// spinlock_unlock (&mm_lock);
 	/* The old_type must be PAGE_TYPE_FREE, or the memory will be
 	 * corrupted.  The ASSERT is called after unlock to avoid
 	 * deadlocks during panic. */
-	//ASSERT (old_type == PAGE_TYPE_FREE);
+	// ASSERT (old_type == PAGE_TYPE_FREE);
 	return p;
 }
 
@@ -215,13 +219,14 @@ mm_page_alloc_cont (int n, int m)
 	int s;
 	virt_t virt;
 	struct page *p, *q;
-	//enum page_type old_ptype, old_qtype;
+	// enum page_type old_ptype, old_qtype;
 
-	//ASSERT (n < NUM_OF_ALLOCSIZE);
-	//ASSERT (m < NUM_OF_ALLOCSIZE);
+	// ASSERT (n < NUM_OF_ALLOCSIZE);
+	// ASSERT (m < NUM_OF_ALLOCSIZE);
 	s = mm_page_get_allocsize (n);
-	//spinlock_lock (&mm_lock);
-	LIST1_FOREACH (list1_freepage[n], p) {
+	// spinlock_lock (&mm_lock);
+	LIST1_FOREACH (list1_freepage[n], p)
+	{
 		virt = page_to_virt (p);
 		if (virt & s)
 			goto not_found;
@@ -234,21 +239,21 @@ mm_page_alloc_cont (int n, int m)
 not_found:
 	p = mm_page_alloc_sub (n + 1);
 	if (!p) {
-		//spinlock_unlock (&mm_lock);
-		//panic ("%s(%d,%d): Out of memory", __func__, n, m);
+		// spinlock_unlock (&mm_lock);
+		// panic ("%s(%d,%d): Out of memory", __func__, n, m);
 	}
 	q = mm_page_free_2ndhalf (p);
 found:
 	LIST1_DEL (list1_freepage[q->allocsize], q);
-	//old_ptype = p->type;
-	//old_qtype = q->type;
+	// old_ptype = p->type;
+	// old_qtype = q->type;
 	p->type = PAGE_TYPE_ALLOCATED_CONT;
 	while (q->allocsize > m)
 		mm_page_free_2ndhalf (q);
 	q->type = PAGE_TYPE_ALLOCATED;
-	//spinlock_unlock (&mm_lock);
-	//ASSERT (old_ptype == PAGE_TYPE_FREE);
-	//ASSERT (old_qtype == PAGE_TYPE_FREE);
+	// spinlock_unlock (&mm_lock);
+	// ASSERT (old_ptype == PAGE_TYPE_FREE);
+	// ASSERT (old_qtype == PAGE_TYPE_FREE);
 	return p;
 }
 
@@ -317,7 +322,7 @@ mm_page_free_sub (struct page *p)
 		LIST1_PUSH (list1_freepage[n], p);
 	while (n < (NUM_OF_ALLOCSIZE - 1) &&
 	       (q = virt_to_page (virt ^ s))->type == PAGE_TYPE_FREE &&
-		q->allocsize == n) {
+	       q->allocsize == n) {
 		if (virt & s) {
 			tmp = p;
 			p = q;
@@ -340,12 +345,12 @@ mm_page_free_sub (struct page *p)
 static void
 mm_page_free (struct page *p)
 {
-	//spinlock_lock (&mm_lock);
+	// spinlock_lock (&mm_lock);
 	char *fail = mm_page_free_sub (p);
-	//spinlock_unlock (&mm_lock);
-    // panic is not implemented
-	//if (fail)
-		//panic ("%s: %s", __func__, fail);
+	// spinlock_unlock (&mm_lock);
+	// panic is not implemented
+	// if (fail)
+	// panic ("%s: %s", __func__, fail);
 }
 
 static int
@@ -375,15 +380,16 @@ small_alloc (int small_allocsize)
 	u32 full = (2U << (nbits - 1)) - 1;
 	u32 n;
 	struct page *p;
-	//spinlock_lock (&mm_small_lock);
-	LIST1_FOREACH (small_freelist[small_allocsize], p) {
+	// spinlock_lock (&mm_small_lock);
+	LIST1_FOREACH (small_freelist[small_allocsize], p)
+	{
 		n = FIND_NEXT_BIT (p->small_bitmap);
 		p->small_bitmap |= n;
 		if (p->small_bitmap == full)
 			LIST1_DEL (small_freelist[small_allocsize], p);
 		goto ok;
 	}
-	//spinlock_unlock (&mm_small_lock);
+	// spinlock_unlock (&mm_small_lock);
 	p = mm_page_alloc (0);
 	ASSERT (p);
 	ASSERT (p->type == PAGE_TYPE_ALLOCATED);
@@ -391,10 +397,10 @@ small_alloc (int small_allocsize)
 	n = 1;
 	p->small_allocsize = small_allocsize;
 	p->small_bitmap = n;
-	//spinlock_lock (&mm_small_lock);
+	// spinlock_lock (&mm_small_lock);
 	LIST1_PUSH (small_freelist[small_allocsize], p);
 ok:
-	//spinlock_unlock (&mm_small_lock);
+	// spinlock_unlock (&mm_small_lock);
 	int i = BIT_TO_INDEX (n);
 	ASSERT (i >= 0);
 	ASSERT (i < nbits);
@@ -412,24 +418,25 @@ tiny_alloc (int tiny_allocsize)
 	ASSERT (sizeof *p <= 2 * TINY_ALLOCSIZE (0));
 	ASSERT (sizeof *p <= TINY_ALLOCSIZE (1));
 	u32 empty_bitmap = tiny_allocsize ? 1 : 3;
-	//spinlock_lock (&mm_tiny_lock);
-	LIST1_FOREACH (tiny_freelist[tiny_allocsize], p) {
+	// spinlock_lock (&mm_tiny_lock);
+	LIST1_FOREACH (tiny_freelist[tiny_allocsize], p)
+	{
 		n = FIND_NEXT_BIT (p->tiny_bitmap);
 		p->tiny_bitmap |= n;
 		if (!~p->tiny_bitmap)
 			LIST1_DEL (tiny_freelist[tiny_allocsize], p);
 		goto ok;
 	}
-	//spinlock_unlock (&mm_tiny_lock);
+	// spinlock_unlock (&mm_tiny_lock);
 	int plen = TINY_ALLOCSIZE (tiny_allocsize) * nbits;
 	p = (void *)small_alloc (get_small_allocsize (plen));
 	n = FIND_NEXT_BIT (empty_bitmap);
 	p->tiny_allocsize = tiny_allocsize;
 	p->tiny_bitmap = empty_bitmap | n;
-	//spinlock_lock (&mm_tiny_lock);
+	// spinlock_lock (&mm_tiny_lock);
 	LIST1_PUSH (tiny_freelist[tiny_allocsize], p);
 ok:
-	//spinlock_unlock (&mm_tiny_lock);
+	// spinlock_unlock (&mm_tiny_lock);
 	int i = BIT_TO_INDEX (n);
 	ASSERT (i > 0);
 	ASSERT (i < nbits);
@@ -459,14 +466,14 @@ small_free (struct page *p, u32 page_offset)
 	int i = page_offset / SMALL_ALLOCSIZE (small_allocsize);
 	ASSERT (i < nbits);
 	u32 n = 1 << i;
-	//spinlock_lock (&mm_small_lock);
+	// spinlock_lock (&mm_small_lock);
 	if (p->small_bitmap == full)
 		LIST1_PUSH (small_freelist[small_allocsize], p);
 	u32 new_small_bitmap = p->small_bitmap ^ n;
 	p->small_bitmap = new_small_bitmap;
 	if (!new_small_bitmap)
 		LIST1_DEL (small_freelist[small_allocsize], p);
-	//spinlock_unlock (&mm_small_lock);
+	// spinlock_unlock (&mm_small_lock);
 	if (new_small_bitmap & n)
 		panic ("%s: double free", __func__);
 	if (!new_small_bitmap) {
@@ -491,14 +498,14 @@ tiny_free (struct page *page, virt_t start_of_block, u32 block_offset)
 	ASSERT (sizeof *p <= TINY_ALLOCSIZE (1));
 	u32 empty_bitmap = tiny_allocsize ? 1 : 3;
 	ASSERT (!(empty_bitmap & n));
-	//spinlock_lock (&mm_tiny_lock);
+	// spinlock_lock (&mm_tiny_lock);
 	if (!~p->tiny_bitmap)
 		LIST1_PUSH (tiny_freelist[tiny_allocsize], p);
 	u32 new_tiny_bitmap = p->tiny_bitmap ^ n;
 	p->tiny_bitmap = new_tiny_bitmap;
 	if (new_tiny_bitmap == empty_bitmap)
 		LIST1_DEL (tiny_freelist[tiny_allocsize], p);
-	//spinlock_unlock (&mm_tiny_lock);
+	// spinlock_unlock (&mm_tiny_lock);
 	if (new_tiny_bitmap & n)
 		panic ("%s: double free", __func__);
 	if (new_tiny_bitmap == empty_bitmap)
@@ -531,8 +538,8 @@ free (void *virt)
 	if (p->type == PAGE_TYPE_ALLOCATED_SMALL) {
 		int small_allocsize = p->small_allocsize;
 		u32 page_offset = v & PAGESIZE_MASK;
-		u32 block_offset = page_offset %
-			SMALL_ALLOCSIZE (small_allocsize);
+		u32 block_offset =
+			page_offset % SMALL_ALLOCSIZE (small_allocsize);
 		if (block_offset)
 			tiny_free (p, v - block_offset, block_offset);
 		else
@@ -545,16 +552,16 @@ free (void *virt)
 static void
 mm_init_global (void)
 {
-    int i;
+	int i;
 
-    vmm_start_phys = (phys_t)phys_base;
+	vmm_start_phys = (phys_t)phys_base;
 
-    // initailize list data structore
+	// initailize list data structore
 	for (i = 0; i < NUM_OF_ALLOCSIZE; i++)
 		LIST1_HEAD_INIT (list1_freepage[i]);
-	//for (i = 0; i < NUM_OF_SMALL_ALLOCSIZE; i++)
+	// for (i = 0; i < NUM_OF_SMALL_ALLOCSIZE; i++)
 	//	LIST1_HEAD_INIT (small_freelist[i]);
-	//for (i = 0; i < NUM_OF_TINY_ALLOCSIZE; i++)
+	// for (i = 0; i < NUM_OF_TINY_ALLOCSIZE; i++)
 	//	LIST1_HEAD_INIT (tiny_freelist[i]);
 	for (i = 0; i < NUM_OF_PAGES; i++) {
 		pagestruct[i].type = PAGE_TYPE_RESERVED;
@@ -566,7 +573,7 @@ mm_init_global (void)
 		if ((u64)(virt_t)head <= page_to_virt (&pagestruct[i]) &&
 		    page_to_virt (&pagestruct[i]) < (u64)(virt_t)end)
 			continue;
-		//if (i < panicmem_start_page + NUM_OF_PANICMEM_PAGES)
+		// if (i < panicmem_start_page + NUM_OF_PANICMEM_PAGES)
 		//	continue;
 		mm_page_free (&pagestruct[i]);
 	}
