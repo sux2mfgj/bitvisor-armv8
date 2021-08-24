18ecee084aa3ee8b71a095d0a96fe4bdee8365fd
Shunsuke Mie <sux2mfgj@gmail.com>
2021-08-24 22:34:43 +0900

    arch: integrate vm.c

--- arch/aarch64/vm.c
+++ arch/aarch64/vm.c
@@ -1,14 +1,31 @@
 
+#include <asm.h>
+#include <core/initfunc.h>
 #include <core/types.h>
 #include <core/vcpu.h>
 
 #include "context.h"
 #include "mmu.h"
+#include "tcr.h"
 
 #define MMU_4K_INDEX_MASK (0b111111111)
 #define MMU_4K_L1_INDEX_OFFSET 30
 #define MMU_4K_L2_INDEX_OFFSET 21
 #define MMU_4K_L3_INDEX_OFFSET 12
+
+extern u8 head[], end[];
+
+void
+vm_mapmem (u64 physaddr, uint len, int flags)
+{
+	not_yet_implemented ();
+}
+
+void
+vm_unmapmem (u64 ptr, uint len)
+{
+	not_yet_implemented ();
+}
 
 static u64 *
 page_walk (u64 *root_pt, int level, u64 phys)
@@ -116,3 +133,84 @@ arch_flush_tlb_entry (phys_t gpst, phys_t gpend)
 	// TODO
 	return true;
 }
+
+static void
+vm_init_global (void)
+{
+	int i;
+
+	int l3_size = PAGESIZE * 512;
+	int l2_size = l3_size * 512;
+	int l1_size = l2_size * 512;
+
+	int l1_index = ((phys_t)head / l1_size) & (512 - 1);
+	int l2_index = ((phys_t)head / l2_size) & (512 - 1);
+	int l3_index = ((phys_t)head / l3_size) & (512 - 1);
+
+	u64 *l1_pt_virt, l1_pt_phys;
+	u64 *l2_pt_virt, l2_pt_phys;
+	u64 *l3_pt_virt, l3_pt_phys;
+
+	alloc_pages ((void **)&l1_pt_virt, &l1_pt_phys, 2);
+	alloc_pages ((void **)&l2_pt_virt, &l2_pt_phys, 2);
+	alloc_pages ((void **)&l3_pt_virt, &l3_pt_phys, 2);
+	memset (l1_pt_virt, 0x00, PAGESIZE * 2);
+	memset (l2_pt_virt, 0x00, PAGESIZE * 2);
+	memset (l3_pt_virt, 0x00, PAGESIZE * 2);
+
+	printf ("0x%llx 0x%llx 0x%llx\n", (u64)l1_pt_phys, (u64)l2_pt_phys,
+		(u64)l3_pt_phys);
+	printf ("l1 0x%x, l2 0x%x l3 0x%x\n", l1_index, l2_index, l3_index);
+
+	l1_pt_virt[l1_index] = (u64)l2_pt_phys | PTE_TABLE | PTE_PRESENT;
+	l2_pt_virt[l2_index] = (u64)l3_pt_phys | PTE_TABLE | PTE_PRESENT;
+	l2_pt_virt[0] = 0x00000000 | PTE_BLOCK | PTE_MRA_DEVICE_nGnRnE |
+			PTE_PRESENT | LOW_ATTR_AF;
+
+	// TODO
+	for (i = 0; i < 512; ++i) {
+		if (i > 440 && i < 460)
+			printf ("%d 0x%x 0x%llx\n", i, i,
+				(0x40000000ULL + (i * l3_size)));
+		l3_pt_virt[i] = (0x40000000ULL + (i * l3_size)) | PTE_BLOCK |
+				PTE_MRA_DEVICE_nGnRnE | PTE_PRESENT |
+				LOW_ATTR_AF;
+	}
+
+	// for (i = 0; i < VMMSIZE_ALL >> PAGESIZE2M_SHIFT; i++) {
+	//	ASSERT (l3_index + i < 512);
+	//	phys_t base = head & -0x200000 + (i * l3_size);
+	//	phys_t base = (phys_t)head + (i * l3_size);
+	//	l3_pt_virt[l3_index + i] = base | PTE_BLOCK |
+	//				   PTE_MRA_DEVICE_nGnRnE | PTE_PRESENT |
+	//				   LOW_ATTR_AF;
+	//}
+
+	// disable mmu
+	u64 sctlr_el2 = read_sctlr_el2 ();
+	sctlr_el2 &= ~0x1; // SCTLR_EL2_MMU_EN
+	write_sctlr_el2 (sctlr_el2);
+
+	// save to ttbr0
+	write_ttbr0_el2 ((u64)l1_pt_phys);
+
+	// setup TCR
+	write_tcr_el2 ((u64)TCR_VALUE);
+
+	// setup mair
+	write_mair_el2 ((u64)MAIR_VALUE);
+
+	// call isb
+	isb ();
+
+	// enable mmu (sctlr_el2)
+	sctlr_el2 = read_sctlr_el2 ();
+	sctlr_el2 |= 0x1; // SCTLR_EL2_MMU_EN
+	write_sctlr_el2 (sctlr_el2);
+
+	// call isb
+	isb ();
+}
+
+// vm_init_global depends on the mm subsystem. It is initialized "global2".
+INITFUNC ("global3", vm_init_global);
--- include/core/vm.h
+++ include/core/vm.h
@@ -6,8 +6,8 @@
 bool arch_flush_tlb_entry (phys_t gpst, phys_t gpend);
 
 /* accessing memory */
-void unmapmem (void *virt, uint len);
-void *mapmem_hphys (u64 physaddr, uint len, int flags);
-void *mapmem_as (const struct mm_as *as, u64 physaddr, uint len, int flags);
+void vm_mapmem (u64 physaddr, uint len, int flags);
+void vm_unmapmem (u64 ptr, uint len);
+
 
 #endif // __CORE_VM_H
